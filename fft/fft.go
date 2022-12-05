1103334bcd49f269be48dfaa679ad2ed328a0b33
Vaibhav Chimalgi <vaibhavchimalgi@Vaibhavs-MacBook-Air.local>
2022-12-06 00:59:10 +0530

    index range fix

--- fft/fft.go
+++ fft/fft.go
@@ -1,6 +1,44 @@
 package fft
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+type Poly []complex128
+
+func FFT(p []complex128) Poly{
+	n := len(p)
+	if n == 1 {
+		return p
+	}
+	// w := cmplx.Exp(complex(0,float64(2*math.Pi)/float64(n)))
+
+	pe := make(Poly, n/2)
+	po := make(Poly, n/2)
+
+	for i:= 0; i<n; i++ {
+		i2 := 0
+		if i%2 == 0 {
+			pe[i2] = p[i]
+		}else{
+			po[i2] = p[i]
+		}
+		i2++
+	}
+
+	ye := FFT(pe)
+	yo := FFT(po)
+
+	y := make(Poly, n)
+	for j:=0;j<n/2;j++ {
+		y[j] = ye[j] + yo[j]*cmplx.Exp(complex(0,float64(2*float64(j)*math.Pi)/float64(n)))
+		y[j+n/2] = ye[j] - yo[j]*cmplx.Exp(complex(0,float64(2*float64(j)*math.Pi)/float64(n)))
+	}
+	return y;
+
+} 
 
 func Hello(name string) string{
 	message := fmt.Sprintf("Hi %v, Welcome.",name)
--- main/main.go
+++ main/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	// "math"
+	// "math/cmplx"
 	"fft"
 )
+
+type Poly []complex128
 
 func main(){
 	message := fft.Hello("Vaibhav")
 	fmt.Println(message)
+	var p Poly
+	p = append(p,complex(1,0))
+	p = append(p,complex(2,0))
+	p = append(p,complex(2,0))
+	p = append(p,complex(3,0))
+	printSlice(p)
+	fmt.Printf("%v",fft.FFT(p))
+}
+
+func printSlice(s Poly) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
