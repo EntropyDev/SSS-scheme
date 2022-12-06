8556b958a3a841bbb0db0d859cd954dfac0dc7c0
Vaibhav Chimalgi <vaibhavchimalgi@Vaibhavs-MacBook-Air.local>
2022-12-06 18:39:39 +0530

    inverse fft imp

--- fft/fft.go
+++ fft/fft.go
@@ -13,7 +13,11 @@ func FFT(p []complex128, f bool) Poly{
 	if n == 1 {
 		return p
 	}
-	w := cmplx.Exp(complex(0,float64(2*math.Pi)/float64(n)))
+	w_n := cmplx.Exp(complex(0,-2*math.Pi/float64(n)))
+	if !f {
+		w_n = 1/w_n
+	}
+	w := complex(1,0)
 	// if !f{
 	// 	w = complex(1/float64(n),0)*cmplx.Pow(cmplx.Exp(complex(0,float64(-2*math.Pi)/float64(n))),-1+0i)
 	// 	fmt.Println("working")
@@ -27,7 +31,7 @@ func FFT(p []complex128, f bool) Poly{
 		if i%2 == 0 {
 			pe[i/2] = p[i]
 		}else{
-			po[(i-1)/2] = p[i]
+			po[i/2] = p[i]
 		}
 	}
 	// fmt.Println(w)
@@ -43,13 +47,15 @@ func FFT(p []complex128, f bool) Poly{
 	y := make(Poly, n)
 	if f{
 		for j:=0;j<n/2;j++ {
-			y[j] = ye[j] + yo[j]*cmplx.Pow(w,complex(float64(j),0))
-			y[j+n/2] = ye[j] - yo[j]*cmplx.Pow(w,complex(float64(j),0))
+			y[j] = ye[j] + yo[j]*w
+			y[j+n/2] = ye[j] - yo[j]*w
+			w = w * w_n
 		}
 	}else{
 		for j:=0;j<n/2;j++ {
-			y[j] = ye[j] + yo[j]*complex(1/float64(n),0)*cmplx.Pow(cmplx.Exp(complex(0,float64(-2*math.Pi)/float64(n))),-1+0i)
-			y[j+n/2] = ye[j] - yo[j]*complex(1/float64(n),0)*cmplx.Pow(cmplx.Exp(complex(0,float64(-2*math.Pi)/float64(n))),-1+0i)
+			y[j] = (ye[j] + yo[j]*w)/2
+			y[j+n/2] = (ye[j] - yo[j]*w)/2
+			w = w * w_n		
 		}
 		
 	}
--- main/main.go
+++ main/main.go
@@ -15,16 +15,16 @@ func main(){
 	// fmt.Println(message)
 	var p Poly
 	p = append(p,complex(8,0))
-	p = append(p,complex(-1,-1))
-	p = append(p,complex(-2,0))
 	p = append(p,complex(-1,1))
+	p = append(p,complex(-2,0))
+	p = append(p,complex(-1,-1))
 	printSlice(p)
 	// fmt.Println(math.Sin(2*math.Pi))
 	// j := float64(0)
 	// n:=4
 	// fmt.Println(complex(math.Cos((2*j*math.Pi)/float64(n)),math.Sin((2*j*math.Pi)/float64(n))))
 
-	fmt.Printf("%v",fft.FFT(p,true))
+	fmt.Printf("%v",fft.FFT(p,false))
 	// fmt.Println(fft.FFT(p))
 }
 
